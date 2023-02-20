17a4a358a26827885389111fb1af94fab4916c20
lorenzo <lorenzotijerina@gmail.com>
2023-02-20 14:24:26 -0500

    union show page

--- frontend/src/components/Unions/UnionDetails.js
+++ frontend/src/components/Unions/UnionDetails.js
@@ -1,4 +1,35 @@
-//Show page for a Union
-//should import NavBar, Griefs, GriefCompose, and GriefBox
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getUnion } from "../../store/union";
+import Griefs from "../Griefs";
+import GriefCompose from "../GriefCompose";
+import GriefBox from "../GriefBox";
+import NavBar from "../NavBar";
 
-// should show name of the union and
+const UnionDetails = () => {
+    const dispatch = useDispatch();
+    const { unionId } = useParams();
+    const union = useSelector((state) => state.union);
+    const [loaded, setLoaded] = useState(false);
+    
+    useEffect(() => {
+        dispatch(getUnion(unionId)).then(() => setLoaded(true));
+    }, [dispatch, unionId]);
+    
+    if (!loaded) {
+        return null;
+    }
+    
+    return (
+        <div>
+        <NavBar />
+        <h1>{union.name}</h1>
+        <Griefs />
+        <GriefCompose />
+        <GriefBox />
+        </div>
+    );
+    }
+
+export default UnionDetails;
