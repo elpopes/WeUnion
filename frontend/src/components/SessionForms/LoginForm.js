79fab26392fe641247c5ee9a05c493aa75ef7a42
lorenzo <lorenzotijerina@gmail.com>
2023-02-20 15:33:23 -0500

    reformat login and signup forms

--- frontend/src/components/SessionForms/LoginForm.js
+++ frontend/src/components/SessionForms/LoginForm.js
@@ -7,7 +7,6 @@ import { login, clearSessionErrors } from "../../store/session";
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [unionName, setUnionName] = useState("");
   const errors = useSelector((state) => state.errors.session);
   const dispatch = useDispatch();
 
@@ -18,20 +17,13 @@ function LoginForm() {
   }, [dispatch]);
 
   const update = (field) => {
-    return (e) => {
-      if (field === "email") {
-        setEmail(e.currentTarget.value);
-      } else if (field === "password") {
-        setPassword(e.currentTarget.value);
-      } else if (field === "unionName") {
-        setUnionName(e.currentTarget.value);
-      }
-    };
+    const setState = field === "email" ? setEmail : setPassword;
+    return (e) => setState(e.currentTarget.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(login({ email, password, unionName }));
+    dispatch(login({ email, password }));
   };
 
   return (
@@ -57,17 +49,6 @@ function LoginForm() {
           placeholder="Password"
         />
       </label>
-      <div className="errors">{errors?.union}</div>
-      <label>
-        <span>Union Name</span>
-        <input
-          type="text"
-          value={unionName}
-          onChange={update("unionName")}
-          placeholder="Union"
-        />
-      </label>
-
       <input type="submit" value="Log In" disabled={!email || !password} />
     </form>
   );
