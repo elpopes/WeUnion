6dc86b59f0646bb9a69d6013b6fac19112e87561
lorenzo <lorenzotijerina@gmail.com>
2023-02-21 16:39:49 -0500

    persisting union show

--- frontend/src/components/Unions/UnionDetails.js
+++ frontend/src/components/Unions/UnionDetails.js
@@ -2,32 +2,42 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchUnion, getUnion } from "../../store/unions";
-// import Griefs from "../Griefs/Griefs";
-// import GriefCompose from "../Griefs/GriefCompose";
-// import GriefBox from "../Griefs/GriefBox";
 
 const UnionDetails = () => {
   const dispatch = useDispatch();
-  const { id } = useParams(); // extract unionId parameter from URL
+  const { id } = useParams();
   const union = useSelector(getUnion(id));
-
-  //   const [loaded, setLoaded] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    dispatch(fetchUnion(id));
-  }, [dispatch, id]);
+    async function fetchUnionData() {
+      try {
+        await dispatch(fetchUnion(id));
+        setLoaded(true);
+      } catch (e) {
+        setError(e);
+      }
+    }
 
-  //   if (!loaded || !union) {
-  //     return null;
-  //   }
+    if (!union && id) {
+      fetchUnionData();
+    } else if (union) {
+      setLoaded(true);
+    }
+  }, [dispatch, id, union]);
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (!loaded || !union) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      <h1>YOUR MAMA</h1>
       <h1>{union.name}</h1>
-      {/* <Griefs />
-      <GriefCompose />
-      <GriefBox /> */}
     </div>
   );
 };
