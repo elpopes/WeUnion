0a2cec71fc143eb358eebebeab8d8d85ec48f4a8
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-02-23 10:27:01 -0500

    add security modifications to twilio stuff

--- send-sms.js
+++ send-sms.js
@@ -1,5 +1,7 @@
-const accountSid = 'AC87936ce2d8d65a75003c3c67ecec5806'
-const authToken = 'c1e67bc2e14f7da91c77a79555cb3e1e'
+require('dotenv').config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const client = require('twilio')(accountSid, authToken);
 
