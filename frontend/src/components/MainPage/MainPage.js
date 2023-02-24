61573c94fd78cc2e7b7d64232b1cb64aba6f5f26
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-02-24 03:07:19 -0500

    style splash page

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -15,27 +15,54 @@ function MainPage() {
             <div className="attribute">-Margaret Mead</div>
           </div>
         </div>
-        <div className="features-container">
-          <ul className="features-list">
-            <li className="main-feature">
-              Create Union Easily
-              <ul className="feature-details">
-                <li>Invite the People to Join</li>
-                <li>Establish Rights</li>
-                <li>Organize Actions</li>
-              </ul>
-            </li>
-            <li className="main-feature">
-              Membership Means
-              <ul className="feature-details">
-                <li>Digital Membership Card</li>
-                <li>Create Grievances</li>
-                <li>Start an Action</li>
-                <li>Vote!</li>
-                <li>Receive Notification</li>
-              </ul>
-            </li>
-          </ul>
+        <div className="feature-1">
+          <div className="feature-1-left">
+            <h2 className="feature-title">Establish Rights</h2>
+          </div>
+          <div className="feature-1-right">
+            <ol className="feature-description">
+              <li>
+                WeUnion enables you to establish your own rights and protections
+                as a group
+              </li>
+              <br />
+              <li>
+                You can easily draft and publish your union's bylaws and
+                governing documents, allowing you to set the rules and standards
+                for your members to follow
+              </li>
+              <br />
+              <li>
+                With WeUnion, you can ensure that your union is held to the
+                highest standards of transparency, accountability, and ethical
+                conduct
+              </li>
+            </ol>
+          </div>
+        </div>
+        <div className="feature-2">
+          <div className="feature-2-left">
+            <ol className="feature-description">
+              <li>
+                WeUnion empowers you to file your grievances and take action
+                against unfair or unethical practices
+              </li>
+              <br />
+              <li>
+                You can easily log your grievances and track their status as
+                they are being resolved
+              </li>
+              <br />
+              <li>
+                With WeUnion, you can also join other unions and access a
+                database of grievances and actions taken by unions across the
+                world
+              </li>
+            </ol>
+          </div>
+          <div className="feature-2-right">
+            <h2 className="feature-title">File Your Grievances</h2>
+          </div>
         </div>
       </div>
       <Footer />
