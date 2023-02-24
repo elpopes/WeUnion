47e6ab1569139a9a28300c22df3eeca9a5f8f2fd
lorenzo <lorenzotijerina@gmail.com>
2023-02-24 04:05:49 -0500

    updates

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -45,23 +45,70 @@ function MainPage() {
             <ol className="feature-description">
               <li>
                 WeUnion empowers you to file your grievances and take action
-                against unfair or unethical practices
+                against unfair or unethical practices.
               </li>
               <br />
               <li>
                 You can easily log your grievances and track their status as
-                they are being resolved
+                they are being resolved.
               </li>
               <br />
               <li>
                 With WeUnion, you can also join other unions and access a
                 database of grievances and actions taken by unions across the
-                world
+                world.
               </li>
             </ol>
           </div>
           <div className="feature-2-right">
             <h2 className="feature-title">File Your Grievances</h2>
+          </div>
+        </div>
+        <div className="feature-1">
+          <div className="feature-1-left">
+            <h2 className="feature-title">Membership</h2>
+          </div>
+          <div className="feature-1-right">
+            <ol className="feature-description">
+              <li>
+                WeUnion provides an easy-to-use membership management system
+                that allows you to invite, manage, and communicate with your
+                members.
+              </li>
+              <br />
+              <li>
+                You can vote on actions, resolved grievances, and keep track of
+                all your union's actions and grievances.
+              </li>
+              <br />
+              <li>
+                You can also receive notifications about important updates,
+                events, and actions taken by your union.
+              </li>
+            </ol>
+          </div>
+        </div>
+        <div className="feature-3">
+          <div className="feature-4-left">
+            <ol className="feature-description">
+              <li>
+                WeUnion is always working to improve and expand our features to
+                meet the needs of our users.
+              </li>
+              <br />
+              <li>
+                We are currently working on adding maps and calendars to help
+                you plan out your actions and events more effectively.
+              </li>
+              <br />
+              <li>
+                This will make it easier to coordinate with your members and
+                take action together to achieve your goals.
+              </li>
+            </ol>
+          </div>
+          <div className="feature-4-right">
+            <h2 className="feature-title">Features in Development</h2>
           </div>
         </div>
       </div>
