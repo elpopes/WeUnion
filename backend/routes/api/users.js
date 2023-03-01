283a354bf8fbe1239429dbaf6c1bc4403e70021d
lorenzo <lorenzotijerina@gmail.com>
2023-03-01 12:45:30 -0500

    complete adding unions array to user state

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -40,7 +40,7 @@ router.get("/current", restoreUser, (req, res) => {
     username: req.user.username,
     profileImageUrl: req.user.profileImageUrl, // <- ADD THIS LINE
     email: req.user.email,
-    union: req.user.union,
+    unions: req.user.unions,
   });
 });
 
