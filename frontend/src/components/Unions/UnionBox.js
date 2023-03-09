d085750970f2277a7676f4036bf7112cb5821fe4
lorenzo <lorenzotijerina@gmail.com>
2023-03-09 15:34:46 -0500

    added links to union page

--- frontend/src/components/Unions/UnionBox.js
+++ frontend/src/components/Unions/UnionBox.js
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function UnionBox({ union: { name, _id } }) {
   const unionId = _id;
   return (
     <div className="union">
       <div className="union-text">
-        <h3>{name}</h3>
+        <h3>
+          <Link to={`/unions/${unionId}`}>{name}</Link>
+        </h3>
+        <Link to={`/unions/${unionId}`}>Visit Union</Link>
       </div>
     </div>
   );
