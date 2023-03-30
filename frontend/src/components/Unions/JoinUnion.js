60afb1921fa76d386bc651c4a92a29b7b0c76a6e
lorenzo <lorenzotijerina@gmail.com>
2023-03-30 14:15:16 -0400

    complete join union component

---
+++ frontend/src/components/Unions/JoinUnion.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import jwtFetch from "../../store/jwt";
+import { useSelector } from "react-redux";
+
+const JoinUnion = ({ union }) => {
+  const unionId = union._id;
+  const user = useSelector((state) => state.session.user);
+
+  const [buttonText, setButtonText] = useState("Join Union");
+
+  const joinUnion = async () => {
+    setButtonText("Joining...");
+
+    try {
+      const res = await jwtFetch(`/union/${unionId}/join`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId: user.id }),
+      });
+
+      if (res.ok) {
+        setButtonText("Joined!");
+      } else {
+        setButtonText("Error");
+      }
+    } catch (e) {
+      console.error(e);
+      setButtonText("Error");
+    }
+  };
+
+  return (
+    <button onClick={joinUnion} disabled={buttonText === "Joined!"}>
+      {buttonText}
+    </button>
+  );
+};
+
+export default JoinUnion;
